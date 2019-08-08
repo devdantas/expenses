edae71dfa91bdb2fd76577e978241eb69b48d618
Maurício Dantas <45770805+devdantas@users.noreply.github.com>
2019-08-08 13:01:04 -0300

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -8,7 +8,7 @@ export const firebaseApp = firebase.initializeApp({
   authDomain: process.env.VUE_APP_AUTH_DOMAIN,
   databaseURL: process.env.VUE_APP_DATABASE_URL,
   projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: 'expenses-72294.appspot.com' /** caso a variavel nao funcione colocar direto */,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET /** caso a variavel nao funcione colocar direto */,
   messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
   appId: process.env.VUE_APP_APP_ID
 })
